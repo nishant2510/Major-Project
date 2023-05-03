871f4174a5d65ce46c55fe60fd22dd601a879e88
Nishant Meena <nishantmeena2510@gmail.com>
2023-05-04 01:41:28 +0530

    Changed project structure

--- repair/repair.js
+++ repair/repair.js
@@ -1,16 +1,12 @@
 var map, places, infoWindow;
 var markers = [];
 var autocomplete;
-var StateRestrict = { 'state': 'all' };
+var StateRestrict = { 'state': 'mh' };
+//list of markers (container for markers)
 var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
 var hostnameRegexp = new RegExp('^https?://.+?/');
 
-// Definig all states lat/long to make the map div responsive when user selects the particular state
 var states = {
-    'all': {
-        center: { lat: 25, lng: 80 },
-        zoom: 5
-    },
     'mh': {
         center: { lat: 19.9915, lng: 76.5139 },
         zoom: 6.9
@@ -120,51 +116,34 @@ var states = {
 
 };
 
-// Creating an instance of maps and showing it in the map div with some default settings
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: states['all'].zoom,
-        center: states['all'].center,
-        mapTypeControl: true,
-        panControl: true,
-        zoomControl: true,
-        streetViewControl: true
+        zoom: states['mh'].zoom,
+        center: states['mh'].center,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: false,
+        streetViewControl: false
     });
 
-    // This will be used to show the info about the place selected by the user
     infoWindow = new google.maps.InfoWindow({
         content: document.getElementById('info-content')
     });
 
-    // Using autocomplete API to suggest the city names to users
-
-    // let state = document.getElementById('state').value
-    // let center = { lat:states[state].center.lat, lng:states[state].center.lng};
-    // const defaultBounds = {
-    //     north: center.lat + 2,
-    //     south: center.lat - 2,
-    //     east: center.lng + 2,
-    //     west: center.lng - 2,
-    // };
-
-    autocomplete = new google.maps.places.Autocomplete((document.getElementById('autocomplete')),
-        {
-            // bounds: defaultBounds,
-            types: ['(cities)'],
-            componentRestrictions: StateRestrict,
-            // strictBounds: true
-        }
-    );
+    autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(
+            document.getElementById('autocomplete')), {
+        types: ['(cities)'],
+        componentRestrictions: StateRestrict
+    });
     places = new google.maps.places.PlacesService(map);
 
-    // To make the map responsive when user selects a city
     autocomplete.addListener('place_changed', onPlaceChanged);
 
-    // Add a event listener to respond when the user selects a state
-    document.getElementById('state').addEventListener('change', setAutocompleteState);
+    document.getElementById('state').addEventListener(
+        'change', setAutocompleteCountry);
 }
 
-// To get the place details for the city selected by the user and zoom it
 function onPlaceChanged() {
     var place = autocomplete.getPlace();
     if (place.geometry) {
@@ -176,31 +155,25 @@ function onPlaceChanged() {
     }
 }
 
-// Search for gas stations in the selected city.
 function search() {
     var search = {
         bounds: map.getBounds(),
-        types: ['gas_station']
+        types: ['car_repair']
     };
 
     places.nearbySearch(search, function (results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             clearResults();
             clearMarkers();
-
-            // Create a marker and assign a alphabet on it for each gas station found
             for (var i = 0; i < results.length; i++) {
                 var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
                 var markerIcon = MARKER_PATH + markerLetter + '.png';
-
                 // Use marker animation to drop the icons incrementally on the map.
                 markers[i] = new google.maps.Marker({
                     position: results[i].geometry.location,
                     animation: google.maps.Animation.DROP,
                     icon: markerIcon
                 });
-
-                // If the user clicks a gas station marker, show the details of that hotel in the info window
                 markers[i].placeResult = results[i];
                 google.maps.event.addListener(markers[i], 'click', showInfoWindow);
                 setTimeout(dropMarker(i), i * 100);
@@ -210,7 +183,6 @@ function search() {
     });
 }
 
-// Helper function to clear out the markers from map
 function clearMarkers() {
     for (var i = 0; i < markers.length; i++) {
         if (markers[i]) {
@@ -220,24 +192,17 @@ function clearMarkers() {
     markers = [];
 }
 
-// Set the state restriction based on user input and center and zoom the map
-function setAutocompleteState() {
+function setAutocompleteCountry() {
     var state = document.getElementById('state').value;
-
-    // if (state == 'all') {
-    //     autocomplete.setComponentRestrictions({ 'state': 'all' });
-    //     map.setCenter({ lat: 25, lng: 80 });
-    //     map.setZoom(5);
-    // } else {
-    //     autocomplete.setComponentRestrictions({ 'state': state });
-    //     map.setCenter(states[state].center);
-    //     map.setZoom(states[state].zoom);
-    // }
-
-    autocomplete.setComponentRestrictions({ 'state': state });
-    map.setCenter(states[state].center);
-    map.setZoom(states[state].zoom);
-
+    if (state == 'all') {
+        autocomplete.setComponentRestrictions({ 'state': [] });
+        map.setCenter({ lat: 15, lng: 0 });
+        map.setZoom(2);
+    } else {
+        autocomplete.setComponentRestrictions({ 'country': 'in' });
+        map.setCenter(states[state].center);
+        map.setZoom(states[state].zoom);
+    }
     clearResults();
     clearMarkers();
 }
@@ -248,20 +213,17 @@ function dropMarker(i) {
     };
 }
 
-// To show all the neares gas stations in list format on the UI
 function addResult(result, i) {
     var results = document.getElementById('results');
     var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
     var markerIcon = MARKER_PATH + markerLetter + '.png';
 
-    // Setting different colors for adjacent items in the list
     var tr = document.createElement('tr');
     tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
     tr.onclick = function () {
         google.maps.event.trigger(markers[i], 'click');
     };
 
-    // Appending items in the list
     var iconTd = document.createElement('td');
     var nameTd = document.createElement('td');
     var icon = document.createElement('img');
@@ -283,7 +245,6 @@ function clearResults() {
     }
 }
 
-// Get and show details for a gas station in info window,
 function showInfoWindow() {
     var marker = this;
     places.getDetails({ placeId: marker.placeResult.place_id },
@@ -293,13 +254,11 @@ function showInfoWindow() {
             }
             infoWindow.open(map, marker);
             buildIWContent(place);
-        }
-    );
+        });
 }
 
-// Load the place information into the HTML elements used by the info window.
 function buildIWContent(place) {
-    document.getElementById('iw-icon').innerHTML = '<img class="gasStationIcon" ' +
+    document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
         'src="' + place.icon + '"/>';
     document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
         '">' + place.name + '</a></b>';
@@ -313,7 +272,6 @@ function buildIWContent(place) {
         document.getElementById('iw-phone-row').style.display = 'none';
     }
 
-    // To assign the rating of the place
     if (place.rating) {
         var ratingHtml = '';
         for (var i = 0; i < 5; i++) {
@@ -329,21 +287,16 @@ function buildIWContent(place) {
         document.getElementById('iw-rating-row').style.display = 'none';
     }
 
-    // To give a short URL for displaying in the info window.
     if (place.website) {
         var fullUrl = place.website;
-
-        // The regexp isolates the first part of the URL (domain plus subdomain)
-        // var website = hostnameRegexp.exec(place.website);
-        // if (website === null) {
-        //     website = 'http://' + place.website + '/';
-        //     fullUrl = website;
-        // }
-
+        var website = hostnameRegexp.exec(place.website);
+        if (website === null) {
+            website = 'http://' + place.website + '/';
+            fullUrl = website;
+        }
         document.getElementById('iw-website-row').style.display = '';
-        document.getElementById('iw-website').textContent = fullUrl;
+        document.getElementById('iw-website').textContent = website;
     } else {
-        document.getElementById('iw-website-row').style.display = '';
-        document.getElementById('iw-website').textContent = 'Website not available';
+        document.getElementById('iw-website-row').style.display = 'none';
     }
 }
